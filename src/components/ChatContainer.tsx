c72666e8fa89ecce7a83b8b838949aa0b6325094
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-11 16:16:58 +0530

    Added bottom area and fixed few css bugs.

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -3,7 +3,10 @@ import assets, { messagesDummyData } from "../assets/assets";
 import type { SelectedUserProps } from "../lib/types";
 import { formatMsgTime } from "../lib/utils";
 
-const ChatContainer = ({ selectedUser }: SelectedUserProps) => {
+const ChatContainer = ({
+  selectedUser,
+  setSelectedUser,
+}: SelectedUserProps) => {
   const scrollEnd = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
@@ -13,16 +16,29 @@ const ChatContainer = ({ selectedUser }: SelectedUserProps) => {
   }, []);
 
   return selectedUser ? (
-    <div className="">
+    // User Area
+    <div className="h-full overflow-scroll relative backdrop-blur-lg">
       <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
         <img src={assets.profile_martin} alt="" className="w-8 rounded-full" />
         <p className="flex-1 text-lg text-white flex items-center gap-2">
           Martin Johnson{" "}
           <span className="w-2 h-2 rounded-full bg-green-500"></span>
         </p>
-        <img src={assets.arrow_icon} alt="" className="md:hidden max-w-7" />
-        <img src={assets.help_icon} alt="" className="max-md:hidden max-w-5" />
+        <img
+          onClick={() => {
+            setSelectedUser(null);
+          }}
+          src={assets.arrow_icon}
+          alt="Arrow"
+          className="md:hidden max-w-7"
+        />
+        <img
+          src={assets.help_icon}
+          alt="Help"
+          className="max-md:hidden max-w-5"
+        />
       </div>
+
       {/* Chat Area */}
       <div className="flex flex-col overflow-y-scroll p-3 pb-6 h-[calc(100%-120px)]">
         {messagesDummyData.map((msg, index) => (
@@ -64,13 +80,37 @@ const ChatContainer = ({ selectedUser }: SelectedUserProps) => {
             </div>
           </div>
         ))}
-
         <div ref={scrollEnd}></div>
+      </div>
+
+      {/* Bottom Area */}
+      <div className="absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3">
+        <div className="flex-1 flex items-center bg-gray-100/12 px-3 rounded-full">
+          <input
+            type="text"
+            placeholder="Send a message"
+            className="flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400"
+          />
+          <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+          <label htmlFor="image">
+            <img
+              src={assets.gallery_icon}
+              alt="upload an image"
+              className="w-5 mr-2 cursor-pointer"
+            />
+          </label>
+        </div>
+        <img
+          src={assets.send_button}
+          alt="Send Message"
+          className="w-7 cursor-pointer"
+        />
       </div>
     </div>
   ) : (
+    // Idle Area
     <div className="flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden">
-      <img src={assets.logo_icon} alt="" className="max-w-16" />
+      <img src={assets.logo_icon} alt="Message Icon" className="max-w-16" />
       <p className="text-lg font-medium text-white">Chat anytime, anywhere</p>
     </div>
   );
