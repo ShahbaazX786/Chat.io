06b47390159b654ae70c670e65ef8c500b12c017
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-11 15:24:22 +0530

    Added dummy data in left side bar comp.

--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -1,7 +1,8 @@
 import { useNavigate } from "react-router-dom";
-import assets from "../assets/assets";
+import assets, { userDummyData } from "../assets/assets";
+import type { SelectedUserProps } from "../lib/types";
 
-const LeftSideBar = ({ selectedUser }: any) => {
+const LeftSideBar = ({ selectedUser, setSelectedUser }: SelectedUserProps) => {
   const navigate = useNavigate();
 
   return (
@@ -45,6 +46,39 @@ const LeftSideBar = ({ selectedUser }: any) => {
             className="bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8]"
           />
         </div>
+      </div>
+
+      <div className="flex flex-col">
+        {userDummyData.map((user, index) => (
+          <div
+            key={index}
+            onClick={() => {
+              setSelectedUser(user);
+            }}
+            className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${
+              selectedUser?._id === user._id && "bg-[#282142]/50"
+            }`}
+          >
+            <img
+              src={user?.profilePic || assets.avatar_icon}
+              alt=""
+              className="w-[35px] aspect-[1/1] rounded-full "
+            />
+            <div className="flex flex-col leading-5">
+              <p className="">{user?.fullName}</p>
+              {index < 3 ? (
+                <span className="text-green-400 text-xs">Online</span>
+              ) : (
+                <span className="text-xs text-neutral-400">Offline</span>
+              )}
+            </div>
+            {index > 2 && (
+              <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">
+                {index}
+              </p>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
