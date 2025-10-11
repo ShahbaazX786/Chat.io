a9086f23bc8bdd3a7b3796dded3eb29053e462f3
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-11 12:56:34 +0530

    Added routes in app file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Bismillah</h1>;
+  // return <h1 className="text-3xl font-bold underline">Bismillah</h1>;
+  return (
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain bg-no-repeat bg-black bg-center">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
