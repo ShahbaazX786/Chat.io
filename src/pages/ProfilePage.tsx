217c9e8329b17fa006a6fce4c9c67f6088e50231
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-13 10:46:33 +0530

    Added Profile Page with navigation and dummy handler logic.

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,5 +1,84 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import assets from "../assets/assets";
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [name, setName] = useState("Random");
+  const [bio, setBio] = useState("Wassup my Random");
+  const navigate = useNavigate();
+
+  const saveHandler = async (e: Event) => {
+    e.preventDefault();
+    navigate("/");
+  };
+
+  return (
+    <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
+      <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
+        <form
+          onSubmit={saveHandler}
+          className="flex flex-col gap-5 p-10 flex-1"
+        >
+          <h3 className="text-lg">Profile Details</h3>
+          <label
+            htmlFor="avatar"
+            className="flex items-center gap-3 cursor-pointer"
+          >
+            <input
+              type="file"
+              name="avatar"
+              id="avatar"
+              accept=".png, .jpg, .jpeg"
+              hidden
+              onChange={(e) => setSelectedImage(e.target.files[0])}
+            />
+            <img
+              src={
+                selectedImage
+                  ? URL.createObjectURL(selectedImage)
+                  : assets.avatar_icon
+              }
+              alt="avatar icon"
+              className={`w-12 h-12 ${selectedImage && "rounded-full"}`}
+            />
+            Upload Profile Image.
+          </label>
+          <input
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            value={name}
+            type="text"
+            required
+            placeholder="Your Name"
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+          />
+          <textarea
+            rows={4}
+            onChange={(e) => {
+              setBio(e.target.value);
+            }}
+            value={bio}
+            required
+            placeholder="Your Profile Bio..."
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer"
+          >
+            Save
+          </button>
+        </form>
+        <img
+          src={assets.logo_icon}
+          alt="Logo Icon"
+          className="max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
