fa0d69865338f29130bdf688d45a6cb257002b2a
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-13 12:38:11 +0530

    added new message controller

---
+++ Backend/controllers/messageController.js
@@ -0,0 +1,24 @@
+import User from '../models/User.js'
+import Message from '../models/Message.js'
+
+// Get all users except the logged in user
+
+
+export const getUsersForSidebar = async () => {
+    try {
+        const userId = req.user._id;
+        const filteredUsers = await User.find({ _id: { $ne: userId } }).select('-password');
+        const unseenMessages = {}
+        const promises = filteredUsers.map(async (user) => {
+            const messages = await Message.find({ senderId: user._id, receiverId: userId, seen: false });
+            if (messages.length > 0) {
+                unseenMessages[user._id] = messages.length;
+            }
+        })
+        await Promise.all(promises);
+        res.json({ success: true, users: filteredUsers, unseenMessages });
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message })
+    }
+}
