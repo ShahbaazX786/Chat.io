b962274d3eb5eb660fc91fb7ee64d7ebdbca91f1
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-14 11:27:21 +0530

    Added new methods in msgcontroller file.

--- Backend/controllers/messageController.js
+++ Backend/controllers/messageController.js
@@ -1,9 +1,8 @@
-import User from '../models/User.js'
-import Message from '../models/Message.js'
+import { uploadImage } from '../lib/utils.js';
+import Message from '../models/Message.js';
+import User from '../models/User.js';
 
 // Get all users except the logged in user
-
-
 export const getUsersForSidebar = async () => {
     try {
         const userId = req.user._id;
@@ -17,6 +16,58 @@ export const getUsersForSidebar = async () => {
         })
         await Promise.all(promises);
         res.json({ success: true, users: filteredUsers, unseenMessages });
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// Get all messages for selected User
+export const getMessages = async (req, res) => {
+    try {
+        const { id: selectedUserId } = req.params;
+        const myId = req.user._id;
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, receiverId: selectedUserId },
+                { senderId: selectedUserId, receiverId: myId }
+            ]
+        })
+        await Message.updateMany({ senderId: selectedUserId, receiverId: myId }, { seen: true });
+        res.json({ success: true, messages })
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// Marks message as seen using MsgID
+export const markMessageAsSeen = async (req) => {
+    try {
+        const { id } = req.params;
+        await Message.findByIdAndUpdate(id, { seen: true })
+        res.json({ success: true })
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// Send Msg to Selected User
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+        const receiverId = req.params.id;
+        const senderId = req.user._id;
+        let imgUrl;
+        if (image) {
+            imgUrl = await uploadImage(image);
+        }
+        const newMsg = await Message.create({
+            senderId, receiverId, text, image: imgUrl
+        })
+        res.json({ success: true, newMsg });
     } catch (error) {
         console.log(error.message);
         res.json({ success: false, message: error.message })
