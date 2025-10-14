bec7c55c5f9e44009e3b248ad20419571c162314
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-14 11:58:19 +0530

    initialided socket io in serverjs file.

--- Backend/server.js
+++ Backend/server.js
@@ -5,20 +5,26 @@ import http from 'http';
 import { connectDB } from './lib/db.js';
 import messageRouter from './routes/messageRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import { initializeSocket } from './lib/socket.js'
 
+// Server 
 const app = express();
 const server = http.createServer(app);
+initializeSocket(server);
 
+// Middleware
 app.use(express.json({ limit: '4mb' }))
 app.use(cors());
 
+// Routes
 app.use('/api/status', (_req, res) => res.send('Server is up and running'));
 app.use('/api/auth', userRouter);
 app.use('/api/messages', messageRouter);
 
+// DB Connection
 await connectDB();
 
-
+// Server Listening
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => { console.log('Server is running on PORT:', PORT) });
