b25b6c1f49b2ed2a65c0e7fe6c06ee281799fb62
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-14 12:41:42 +0530

    initialized toaster in app file.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   // return <h1 className="text-3xl font-bold underline">Bismillah</h1>;
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain bg-no-repeat bg-black bg-center">
+      <Toaster />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
