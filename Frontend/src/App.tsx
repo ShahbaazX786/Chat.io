e84bc3405f9178dd66df202ff02666874323e853
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-14 12:48:48 +0530

    utilized auth context in app file t osecure routes.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,18 +1,32 @@
-import { Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { Toaster } from "react-hot-toast";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
-import { Toaster } from "react-hot-toast";
 
 function App() {
   // return <h1 className="text-3xl font-bold underline">Bismillah</h1>;
+
+  const { authUser } = useContext(AuthContext);
+
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain bg-no-repeat bg-black bg-center">
       <Toaster />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to={`/login`} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={`/`} />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to={`/login`} />}
+        />
       </Routes>
     </div>
   );
