04ecb07ad0617b3227568f086d1a18e87d0442bf
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-16 15:44:12 +0530

    Added 2 new util functions.

--- Frontend/src/lib/utils.ts
+++ Frontend/src/lib/utils.ts
@@ -5,3 +5,41 @@ export function formatMsgTime(date: string) {
     hour12: true,
   });
 }
+
+const getMemberSince = (date: string): string => {
+  const createdAt = new Date(date);
+  const today = new Date();
+
+  let years = today.getFullYear() - createdAt.getFullYear();
+  let months = today.getMonth() - createdAt.getMonth();
+  let days = today.getDate() - createdAt.getDate();
+
+  // Adjusting if days gets negative.
+  if (days < 0) {
+    months -= 1;
+    const prevMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+    days += prevMonth.getDate();
+  }
+
+  if (months < 0) {
+    years -= 1;
+    months += 12;
+  }
+
+  const parts = [];
+  if (years > 0) parts.push(`${years} Year${years > 1 ? "s" : ""}`);
+  if (months > 0) parts.push(`${months} Month${months > 1 ? "s" : ""}`);
+  if (days > 0 || parts.length === 0)
+    parts.push(`${days} Day${days > 1 ? "s" : ""}`);
+
+  if (parts.length === 1) return parts[0];
+  if (parts.length === 2) return `${parts[0]} and ${parts[1]}`;
+  return `${parts[0]}, ${parts[1]}, and ${parts[2]}`;
+};
+
+const getMemberStatus = () => {
+  const status = ["Pro", "Free", "Plus"];
+  return status[Math.floor(Math.random() * status.length)];
+};
+
+export { getMemberSince, getMemberStatus };
